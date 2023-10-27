c119c324dbef0f19745acfceac0ad3940f5ad917
surajverma2587 <surajverma2587@gmail.com>
2023-10-27 18:55:25 +0100

    styled repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,3 +1,20 @@
+import Stack from "react-bootstrap/Stack";
+import { RepoCard } from "./RepoCard";
+
 export const Repos = () => {
-  return <div>Repos</div>;
+  return (
+    <Stack gap={3} className="card m-2 p-4">
+      <h3 className="text-center">Search Results</h3>
+      <Stack
+        gap={2}
+        direction="horizontal"
+        className="flex-wrap justify-content-evenly"
+      >
+        <RepoCard />
+        <RepoCard />
+        <RepoCard />
+        <RepoCard />
+      </Stack>
+    </Stack>
+  );
 };
